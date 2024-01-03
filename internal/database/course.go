d3f0d287432fd84c447806d0ebf469a5a709f700
danielzinhors <danielzinhors@gmail.com>
2024-01-03 20:13:42 -0300

    criado mutation de Course

--- internal/database/course.go
+++ internal/database/course.go
@@ -33,7 +33,7 @@ func (c *Course) Create(name, description, categoryID string) (*Course, error) {
 }
 
 func (c *Course) FindAll() ([]Course, error) {
-	rows, err := c.db.Query("SELECT id, name, description category_id from courses")
+	rows, err := c.db.Query("SELECT id, name, description, category_id from courses")
 	if err != nil {
 		return nil, err
 	}
